9b3ba0cbf02241c67a19ad21d36afa1c2ba41ab2
CHia <dozeuto@gmail.com>
2024-06-17 15:51:07 +0800

    complete translatation function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,84 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleTranslate = () => {
+    if (input.length === 0) {
+      return;
+    }
+
+    setLoading(true);
+    axios
+      .post("http://localhost:8000/translate", { content: input })
+      .then((response) => {
+        console.log(response.data);
+        const data = response.data;
+        setResult(data["result"]);
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+        console.log(e.message);
+      });
+  };
+
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="w-1/2 bg-blue-100 h-full">
-        <p>what's this one</p>
+      <div className="py-5 w-1/2 bg-blue-100 h-full flex flex-col items-center">
+        <p>Translate GPT</p>
+        <div className="w-full flex flex-col items-center">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your message
+          </label>
+          <textarea
+            id="message"
+            rows={4}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 min-h-[320px] max-h-[960px]"
+            placeholder="Write your thoughts here..."
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
+        </div>
+        <button
+          type="button"
+          className="my-5 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          onClick={() => handleTranslate()}
+        >
+          翻译
+        </button>
+
+        {loading ? <Spinner /> : <p>{result}</p>}
       </div>
+    </div>
+  );
+}
+
+function Spinner() {
+  return (
+    <div role="status">
+      <svg
+        aria-hidden="true"
+        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span className="sr-only">Loading...</span>
     </div>
   );
 }
